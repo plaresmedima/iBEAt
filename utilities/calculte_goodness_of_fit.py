df695853cf6a58023a90e9a1695efd9309aaf224
JoaoPeriquito <joao.s.periquito@gmail.com>
2024-06-11 12:08:29 +0100

    updated goodness of fit

--- utilities/calculte_goodness_of_fit.py
+++ utilities/calculte_goodness_of_fit.py
@@ -3,9 +3,9 @@ import numpy as np
 def r_square(signal,fit):
 
     residuals = signal - fit
-    ss_res = np.sum(residuals**2)
-    ss_tot = np.sum((signal-np.mean(signal))**2)
-    r_squared = 1 - (ss_res / ss_tot)
+    # ss_res = np.sum(residuals**2)
+    # ss_tot = np.sum((signal-np.mean(signal))**2)
+    # r_squared = 1 - (ss_res / ss_tot)
 
+    return 100*np.sum(residuals**2)/np.sum(signal**2)
 
-    return r_squared
