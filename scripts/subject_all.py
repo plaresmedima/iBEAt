4cc40cbdd5773f7305bafe37227a3f855ea38aaa
JoaoPeriquito <joao.s.periquito@gmail.com>
2024-06-16 16:03:28 +0100

    fixed drive upload bug

--- scripts/subject_all.py
+++ scripts/subject_all.py
@@ -31,7 +31,7 @@ def single_subject(username, password, path, dataset):
     database.log("Analysis of " + pathScan.split('//')[-1] + " has started!")
     database.log("CPU cores: " + str(UsedCores))
     
-    ## HARMONIZATION
+    # HARMONIZATION
 
     steps.rename_all_series(database)
     steps.harmonize_pc(database)
@@ -42,7 +42,7 @@ def single_subject(username, password, path, dataset):
     steps.harmonize_dce(database)
     steps.harmonize_subject_name(database)
     
-    ## SEGMENTATION
+    # ## SEGMENTATION
 
     steps.fetch_dl_models(database)
     steps.fetch_kidney_masks(database)
@@ -53,42 +53,42 @@ def single_subject(username, password, path, dataset):
     steps.compute_whole_kidney_canvas(database)
     steps.export_segmentations(database) 
 
-    ## MOTION CORRECTION
+    # ## MOTION CORRECTION
 
     steps.mdreg_t1(database)
     steps.mdreg_t2(database)
     steps.mdreg_t2star(database)
     steps.mdreg_mt(database)
-    #steps.mdreg_ivim(database)
+    steps.mdreg_ivim(database)
     steps.mdreg_dti(database)
     steps.mdreg_dce(database)
     steps.export_mdreg(database)
 
-    # MAPPING
+    # # MAPPING
 
     steps.map_T1(database)
     steps.map_T2(database)
     steps.map_T2star(database)
     steps.map_MT(database)
-    #steps.map_IVIM(database)
+    steps.map_IVIM(database)
     steps.map_DTI(database)
     steps.map_DCE(database)
     steps.export_mapping(database)
 
-    # ALIGNMENT
+    # # ALIGNMENT
 
     steps.align_dixon(database) 
     steps.align_T1(database)
     steps.align_T2(database)
     steps.align_T2star(database)
     steps.align_MT(database)
-    #steps.align_IVIM(database)
+    steps.align_IVIM(database)
     steps.align_DTI(database)
     steps.align_DCE(database)
-    #steps.align_ASL(database)
+    steps.align_ASL(database)
     steps.export_alignment(database)
 
-    # MEASUREMENT
+    # # MEASUREMENT
 
     steps.fill_gaps(database)
     steps.cortex_medulla(database)
@@ -99,12 +99,12 @@ def single_subject(username, password, path, dataset):
     steps.measure_t2_maps(database)
     steps.measure_t2star_maps(database)
     steps.measure_mt_maps(database)
-    #steps.measure_ivim_maps(database)
+    steps.measure_ivim_maps(database)
     steps.measure_dti_maps(database)
-    #steps.measure_asl_maps(database)
+    steps.measure_asl_maps(database)
     steps.measure_dce_maps(database)
 
-    # ROI analysis
+    # # ROI analysis
 
     steps.roi_fit_T1(database)
     steps.roi_fit_T2(database)
@@ -117,5 +117,5 @@ def single_subject(username, password, path, dataset):
         
     #upload images, logfile and csv to google drive
     #upload.main(pathScan, filename_log, filename_csv)
-    filename_csv = os.path.join(database.path() + '_output', 'biomarkers.csv')
+    filename_csv = os.path.join(database.path() + '_output',database.PatientName[0] + '_biomarkers.csv')
     upload.main(pathScan, filename_log, filename_csv)
